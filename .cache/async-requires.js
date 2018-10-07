fbec96698652e66ad9b9d11d2880c1f8bff31956
Tusharika Sharma <tusharikasharma@tusharikas-mbp.attlocal.net>
2018-10-07 14:24:52 -0500

    pushed updated code

--- .cache/async-requires.js
+++ .cache/async-requires.js
@@ -2,6 +2,7 @@
 const preferDefault = m => m && m.default || m
 
 exports.components = {
+  "component---src-templates-blog-post-js": () => import("/Users/tusharikasharma/project/demo/Pandas/src/templates/blog-post.js" /* webpackChunkName: "component---src-templates-blog-post-js" */),
   "component---cache-dev-404-page-js": () => import("/Users/tusharikasharma/project/demo/Pandas/.cache/dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */),
   "component---src-pages-about-js": () => import("/Users/tusharikasharma/project/demo/Pandas/src/pages/about.js" /* webpackChunkName: "component---src-pages-about-js" */),
   "component---src-pages-index-js": () => import("/Users/tusharikasharma/project/demo/Pandas/src/pages/index.js" /* webpackChunkName: "component---src-pages-index-js" */),
--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -3,6 +3,7 @@ const preferDefault = m => m && m.default || m
 
 
 exports.components = {
+  "component---src-templates-blog-post-js": preferDefault(require("/Users/tusharikasharma/project/demo/Pandas/src/templates/blog-post.js")),
   "component---cache-dev-404-page-js": preferDefault(require("/Users/tusharikasharma/project/demo/Pandas/.cache/dev-404-page.js")),
   "component---src-pages-about-js": preferDefault(require("/Users/tusharikasharma/project/demo/Pandas/src/pages/about.js")),
   "component---src-pages-index-js": preferDefault(require("/Users/tusharikasharma/project/demo/Pandas/src/pages/index.js")),
